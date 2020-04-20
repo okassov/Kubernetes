0b01fafb8130cbf23916a22b8403b5860c49caf9
Okassov Marat <34717508+okassov@users.noreply.github.com>
2020-04-20 18:47:00 +0600

    Update vsphere-uuid-script.sh

--- storage/vSphere/vsphere-uuid-script.sh
+++ storage/vSphere/vsphere-uuid-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export GOVC_USERNAME='kube-pv@technodom.kz'
+export GOVC_USERNAME='kube-pv@vsphere.local'
 export GOVC_INSECURE=1
 export GOVC_PASSWORD='pass'
-export GOVC_URL='vcsa-co.technodom.kz'
-DATACENTER='TD'
-FOLDER='stage-kube'
+export GOVC_URL='vCenter FQDN or IP'
+DATACENTER='Name of DC'
+FOLDER='Name of folder where located your kube vm's'
 IFS=$'\n'
 for vm in $(govc ls "/$DATACENTER/vm/$FOLDER"); do
   MACHINE_INFO=$(govc vm.info -json -dc=$DATACENTER -vm.ipath="/$vm" -e=true)
@@ -15,5 +15,5 @@ for vm in $(govc ls "/$DATACENTER/vm/$FOLDER"); do
   VM_UUID=$( jq -r ' .VirtualMachines[] | .Config.Uuid' <<< $MACHINE_INFO | awk '{print toupper($0)}')
   echo "Patching $VM_NAME with UUID:$VM_UUID"
   # This is done using dry-run to avoid possible mistakes, remove when you are confident you got everything right.
-  kubectl patch node $VM_NAME.technodom.kz -p "{\"spec\":{\"providerID\":\"vsphere://$VM_UUID\"}}"
+  kubectl patch node $VM_NAME -p "{\"spec\":{\"providerID\":\"vsphere://$VM_UUID\"}}"
 done
